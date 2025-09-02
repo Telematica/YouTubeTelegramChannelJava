65d633e87828d86b526afc8d3187510712c357c6
Telematica <linuxeron@gmail.com>
2025-09-02 17:21:15 -0600

    chore(): Refactoring push-to-server script file.

--- scripts/push-to-server.sh
+++ scripts/push-to-server.sh
@@ -1,10 +1,11 @@
 #!/bin/zsh
 
-REPO_DIR=$HOME/YouTubeTelegramChannelJava
-REMOTE_DIR=/Users/telematica/YouTubeTelegramChannelJava/target
+printf "Enter IP Address: " && read -r IP_ADDRESS
+printf "Enter username: " && read -r USER
 
-printf "Enter IP Address: " && read -r IP_ADDRESS && \
-scp \
-$REPO_DIR/target/YouTubeTelegramChannelJava-1.0-SNAPSHOT-jar-with-dependencies.jar \
-telematica@$IP_ADDRESS:$REMOTE_DIR/YouTubeTelegramChannelJava-1.0-SNAPSHOT-jar-with-dependencies.jar && \
-echo "Successfully pushed to server at $IP_ADDRESS"
+REPO_DIR=$HOME/YouTubeTelegramChannelJava
+REMOTE_DIR=/Users/$USER/YouTubeTelegramChannelJava/target
+JAR_FILE=YouTubeTelegramChannelJava-1.0-SNAPSHOT-jar-with-dependencies.jar
+
+scp $REPO_DIR/target/$JAR_FILE $USER@$IP_ADDRESS:$REMOTE_DIR/$JAR_FILE && \
+echo "Successfully pushed to server at $IP_ADDRESS."
